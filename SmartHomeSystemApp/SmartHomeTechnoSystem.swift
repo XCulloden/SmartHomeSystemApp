254acbadb0209935869e0a054b7167655dbb3aa4
Xan Culloden <Culloden.x@etoncollege.org.uk>
2023-11-24 17:10:09 +0000

    beguned

---
+++ SmartHomeSystemApp/SmartHomeTechnoSystem.swift
@@ -0,0 +1,26 @@
+//
+//  SmartHomeTechnoSystem.swift
+//  SmartHomeSystemApp
+//
+//  Created by Culloden, Xan (EJNR) on 24/11/2023.
+//
+
+import Foundation
+
+class device{
+    var name: String = ""
+    var state: Bool = false
+    
+    
+}
+class lighting:device{
+    
+    
+}
+class speaker:device{
+    
+    
+}
+class utility:device{
+    
+}
---
+++ SmartHomeSystemApp/lighting.swift
@@ -0,0 +1,9 @@
+//
+//  lighting.swift
+//  SmartHomeSystemApp
+//
+//  Created by Culloden, Xan (EJNR) on 24/11/2023.
+//
+
+import Foundation
+
